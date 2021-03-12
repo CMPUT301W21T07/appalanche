18c519a483a4af15c6e6a69609c52728326fd7d0
Aryan Pramanick <apramani>
2021-03-11 17:14:30 -0700

    mock class created

---
+++ app/src/main/java/com/team007/appalanche/QR _Code.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class QR_Code extends Experimenter{
+    private string trialType;
+    public string getTrialType(){
+        /* Covert the scanned QR code into a string*/
+    }
+    public openCamera(){
+        /* Opens the camera of the device to scan the QR code after asking permission from the user*/
+    }
+    public saveQRcode(){
+        /*after scanning it saves the QR code for further use/modification.*/
+    }
+}
