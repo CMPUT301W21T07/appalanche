a752e19ff4a158813056a97ef3f819f99b585afd
Aryan Pramanick <apramani>
2021-03-11 17:26:02 -0700

    mock class created

---
+++ app/src/main/java/com/team007/appalanche/Experimenter.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+public class Experimenter extends User{
+    private ArrayList<QRCode> replies = new ArrayList<QRCode>();
+    public void scanQR(QRCode code)
+} 
---
+++ app/src/main/java/com/team007/appalanche/User.java
@@ -0,0 +1,3 @@
+public class User{
+    private string id;
+}
