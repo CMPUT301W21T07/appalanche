a1522dd68b050a3c965b2f5da6d07a1b1e50b4cf
Nadeen <nadeen@ualberta.ca>
2021-03-14 19:36:34 -0600

    add all the barcode model classes and dummy classes

---
+++ app/src/main/java/com/team007/appalanche/NonNegCountTrial.java
@@ -0,0 +1,13 @@
+package com.team007.appalanche;
+
+import java.util.Date;
+
+public class NonNegCountTrial extends Trial{
+    private int count;
+
+    public NonNegCountTrial(Date date, Experimenter userAddedTrial, Location location
+            , int count) {
+        super(date, userAddedTrial, location);
+        this.count = count;
+    }
+}
