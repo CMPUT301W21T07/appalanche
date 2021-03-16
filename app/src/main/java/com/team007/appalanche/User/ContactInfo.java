44840b0d7dd9a2a21290a4a25f3162dee4cb9074
davedoescode <mddavies@ualberta.ca>
2021-03-15 20:26:46 -0600

    create User, Profile, and Contact info classes

---
+++ app/src/main/java/com/team007/appalanche/User/ContactInfo.java
@@ -0,0 +1,26 @@
+package com.team007.appalanche.User;
+
+public class ContactInfo {
+    private Integer phoneNumber;
+    private String githubLink;
+
+    /**
+     * constructor function for contact info
+     * @param phoneNumber
+     * @param githubLink
+     */
+    ContactInfo(Integer phoneNumber, String githubLink) {
+        this.phoneNumber = phoneNumber;
+        this.githubLink = githubLink;
+    }
+
+    /**
+     * function to set contact info
+     * @param phoneNumber
+     * @param githubLink
+     */
+    public void setContactInfo(Integer phoneNumber, String githubLink) {
+        this.phoneNumber = phoneNumber;
+        this.githubLink = githubLink;
+    }
+}
