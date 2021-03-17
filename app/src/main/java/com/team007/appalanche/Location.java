674252117a72b3afab21d5ef8c9a5bdf21c5329e
Aryan Pramanick <apramani>
2021-03-17 17:15:52 -0600

    Location class

--- app/src/main/java/com/team007/appalanche/Location.java
+++ app/src/main/java/com/team007/appalanche/Location.java
@@ -1,17 +1,17 @@
 public class Location {
     private String latitude;
     private String longitude;
-    private String location;
+    private String locationString;
 
 
     /* constructor for class location*/
     public void setLocation(String latitude, String longitude, String locationstring) {
         this.latitude = latitude;
         this.longitude = longitude;
-        this.location = location;
+        this.locationString = location;
     }
     /*This fuction returns the location of the experiment*/
     public String getLocation(){
-    return location;
+    return locationString;
     }
 }
