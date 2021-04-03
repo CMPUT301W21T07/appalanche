3e89a86ff5996bbced8105a91a6e8b5b2b86ac12
davedoescode <mddavies@ualberta.ca>
2021-04-03 16:41:29 -0600

    get edit user info fragment working

--- app/src/main/java/com/team007/appalanche/view/profile/OwnerProfileActivity.java
+++ app/src/main/java/com/team007/appalanche/view/profile/OwnerProfileActivity.java
@@ -1,16 +1,26 @@
 package com.team007.appalanche.view.profile;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.team007.appalanche.R;
+import com.team007.appalanche.experiment.Experiment;
 import com.team007.appalanche.user.User;
+import com.team007.appalanche.view.AddExperimentFragment;
+import com.team007.appalanche.view.EditUserInfoFragment;
 
-public class OwnerProfileActivity extends AppCompatActivity {
+public class OwnerProfileActivity extends AppCompatActivity implements EditUserInfoFragment.OnFragmentInteractionListener {
     private User currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,21 @@ public class OwnerProfileActivity extends AppCompatActivity {
         TextView userID = findViewById(R.id.userID);
         userID.setText(currentUser.getId());
 
+        // Add edit user button listeners
+        Button editUserButton = findViewById(R.id.editProfile);
+        // String finalUserKey = userKey;
+        editUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new EditUserInfoFragment().newInstance(currentUser).show(getSupportFragmentManager(), "New");
+            }
+        });
+
+    }
+
+    @Override
+    public void updateUserInfo(User user) {
+        //TODO: figure out what to do with this function
     }
 
 //    @Override
