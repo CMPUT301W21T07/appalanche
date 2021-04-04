5f79cf0afcc461508d94aaa36b8220624f8f7801
Théo <68571255+FearlessSeagull@users.noreply.github.com>
2021-04-03 20:22:10 -0600

    Implemented search activity (bugs present)

---
+++ app/src/main/java/com/team007/appalanche/view/searching/SearchActivity.java
@@ -0,0 +1,67 @@
+package com.team007.appalanche.view.searching;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.team007.appalanche.R;
+import com.team007.appalanche.custom.CustomList;
+import com.team007.appalanche.experiment.Experiment;
+
+import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
+
+public class SearchActivity extends AppCompatActivity {
+
+    public ListView expList;
+    ArrayAdapter<Experiment> expAdapter;
+    ArrayList<Experiment> ExperimentDataList;
+    FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        setUpFirebase();
+
+        expList = findViewById(R.id.all_experiments);
+        ExperimentDataList = new ArrayList<Experiment>();
+        expAdapter = new CustomList(this, ExperimentDataList);
+        expList.setAdapter(expAdapter);
+    }
+
+    public void setUpFirebase(){
+        db = FirebaseFirestore.getInstance();
+        final CollectionReference collection = db.collection("Experiments");
+        collection.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+                ExperimentDataList.clear(); // just a test
+                // for every document in this collection path
+                for (QueryDocumentSnapshot doc : queryDocumentSnapshots){
+                    Log.d(TAG, String.valueOf(doc.getData().get("description")));
+                    String description = doc.getId();
+                    String trialType = (String) doc.getData().get("trialType");
+                    Boolean expOpen =  doc.getBoolean("expOpen");
+                    Long minNumTrials  = (Long)    doc.getData().get("minNumTrials");
+                    String expOwnerID = (String) doc.getData().get("expOwnerID");
+                    String region = (String) doc.getData().get("region");
+                    Boolean locationRequired =  doc.getBoolean("locationRequired");
+                    Experiment newExp = new Experiment(description, region, trialType, minNumTrials.intValue(), locationRequired, expOpen, expOwnerID );
+                }
+                expAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
