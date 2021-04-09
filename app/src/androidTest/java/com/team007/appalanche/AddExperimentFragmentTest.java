8a5d6f2f5a5552f73a6c3361149390a3b98c1c3a
Aryan Pramanick <apramani>
2021-04-09 15:39:08 -0600

    add experiment test

--- app/src/androidTest/java/com/team007/appalanche/AddExperimentFragmentTest.java
+++ app/src/androidTest/java/com/team007/appalanche/AddExperimentFragmentTest.java
@@ -1,30 +1,48 @@
 package com.team007.appalanche;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.EditText;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
-import com.robotium.solo.Solo;
-import com.team007.appalanche.view.ui.mainActivity.MainActivity;
 
+import com.robotium.solo.Solo;
+import com.team007.appalanche.controller.ExperimentController;
+import com.team007.appalanche.experiment.Experiment;
+import com.team007.appalanche.experiment.MeasurementExperiment;
+import com.team007.appalanche.user.User;
+import com.team007.appalanche.view.experimentActivity.ExperimentActivity;
+import com.team007.appalanche.view.ui.mainActivity.MainActivity;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.UUID; // https://stackoverflow.com/a/41762
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
 
 public class AddExperimentFragmentTest {
     private Solo solo;
+    SharedPreferences.Editor preferencesEditor;
+    SharedPreferences sharedPref;
+    ExperimentController experimentController;
 
     @Rule
     public ActivityTestRule<MainActivity> rule =
             new ActivityTestRule<>(MainActivity.class,true,true);
 
     @Before
-    public void Setup(){
-        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+        Context targetContext = getInstrumentation().getTargetContext();
+        sharedPref = rule.getActivity().getPreferences(Context.MODE_PRIVATE);
+        preferencesEditor = sharedPref.edit();
     }
 
     @Test
@@ -34,19 +52,27 @@ public class AddExperimentFragmentTest {
 
     @Test
     public void AddCountExperimentTest(){
+        preferencesEditor.putString("com.team007.Appalanche.user_key",null );
+        preferencesEditor.commit();
+        assertEquals(sharedPref.getString("com.team007.Appalanche.user_key", null),null);
+
         solo.assertCurrentActivity("Wrong Activity",MainActivity.class);
+
         solo.clickOnView(solo.getView(R.id.addExperimentButton));
-        solo.enterText((EditText) solo.getView(R.id.expDescription),"Experiment1");
+        solo.enterText((EditText) solo.getView(R.id.expDescription),"Experiment2");
         solo.enterText((EditText) solo.getView(R.id.expRegion),"Edmonton");
         solo.enterText((EditText) solo.getView(R.id.expTrials),"4");
         solo.clickOnView(solo.getView(R.id.expType));
         solo.clickOnText("COUNT");
         solo.clickOnButton("Post");
-        assertTrue(solo.searchText("Experiment1"));
+        assertTrue(solo.searchText("Experiment2"));
     }
 
     @Test
     public void AddNonNegativeCountExperimentTest(){
+        preferencesEditor.putString("com.team007.Appalanche.user_key","@8337" );
+        preferencesEditor.commit();
+        assertEquals(sharedPref.getString("com.team007.Appalanche.user_key", null),"@8337" );
         solo.assertCurrentActivity("Wrong Activity",MainActivity.class);
         solo.clickOnView(solo.getView(R.id.addExperimentButton));
         solo.enterText((EditText) solo.getView(R.id.expDescription),"Experiment2");
@@ -60,6 +86,9 @@ public class AddExperimentFragmentTest {
 
     @Test
     public void AddBinomialExperimentTest() {
+        preferencesEditor.putString("com.team007.Appalanche.user_key","@8337" );
+        preferencesEditor.commit();
+        assertEquals(sharedPref.getString("com.team007.Appalanche.user_key", null),"@8337" );
         solo.assertCurrentActivity("Wrong Activity",MainActivity.class);
         solo.clickOnView(solo.getView(R.id.addExperimentButton));
         solo.enterText((EditText) solo.getView(R.id.expDescription),"Experiment3");
@@ -73,6 +102,9 @@ public class AddExperimentFragmentTest {
 
     @Test
     public void AddMeasurementExperimentTest(){
+        preferencesEditor.putString("com.team007.Appalanche.user_key","@8337" );
+        preferencesEditor.commit();
+        assertEquals(sharedPref.getString("com.team007.Appalanche.user_key", null),"@8337" );
         solo.assertCurrentActivity("Wrong Activity",MainActivity.class);
         solo.clickOnView(solo.getView(R.id.addExperimentButton));
         solo.enterText((EditText) solo.getView(R.id.expDescription),"Experiment4");
