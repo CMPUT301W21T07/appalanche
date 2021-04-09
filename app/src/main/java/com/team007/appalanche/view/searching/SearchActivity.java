8464fec761b6dac2692796ee6725b65064d0ebc9
Théo <68571255+FearlessSeagull@users.noreply.github.com>
2021-04-08 22:14:11 -0600

    Added back button to SearchActivity

--- app/src/main/java/com/team007/appalanche/view/searching/SearchActivity.java
+++ app/src/main/java/com/team007/appalanche/view/searching/SearchActivity.java
@@ -54,6 +54,7 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         expDataList = new ArrayList<Experiment>();
         allExperiments = new ArrayList<Experiment>();
